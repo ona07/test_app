49baacb14411782b08f615cfa4e1d40f973227cf
ona07 <b1022096@fun.ac.jp>
2023-08-20 13:31:09 +0900

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
+<!-- src/App.vue -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <sidebar></sidebar>
+    <router-view></router-view>    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Sidebar
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin:0;
+  padding: 0;
+  display: flex;
+  overflow: hidden;
+}
+.app{
+  height: 100vh;
+  background-color: darkgrey;
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+    <aside class="sidebar">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/projects">Projects</router-link>
+    </aside>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MySidebar'
+  };
+  </script>
+  
+  <style scoped>
+  .sidebar {
+    width: 4%;
+    height: 100%;
+    text-align: center;
+    padding: 20px;
+  }
+  
+  .sidebar a {
+    display: block;
+    margin-bottom: 10px;
+    color: #333;
+    text-decoration: none;
+  }
+  </style>
+  
