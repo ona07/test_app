2654f1972ee35e081f931f9bf9ec68a0211046d9
codepro2020MBP04 <codepro2020mbp04@codepro2020MBP04s-MacBook-Pro.local>
2024-09-17 21:57:12 +0900

    deproy

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/product-frontend/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/naohp/' : '/',
   outputDir: 'docs'
 });
